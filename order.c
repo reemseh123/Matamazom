23f159b79e865368edc54f45fc74f5bbb3baf0ff
saadimanar <49749814+saadimanar@users.noreply.github.com>
2019-11-30 12:03:57 +0200

    Create order.c

---
+++ order.c
@@ -0,0 +1,68 @@
+//
+// Created by manar on 25/11/2019.
+//
+#include "product.h"
+#include <stdlib.h>
+#include "Order.h"
+#include <string.h>
+#include "product.h"
+#include "amount_set.h"
+#include <stdio.h>
+#include <malloc.h>
+
+struct order_t{
+    unsigned int id;
+    AmountSet orderedProducts;
+};
+
+
+Order orderCreate(unsigned int id){
+    Order order = malloc(sizeof(*order));
+    if(!order){
+        return NULL;
+    }
+    order->orderedProducts=asCreate((CopyASElement)productCopy, (FreeASElement)productDestroy,
+                                    (CompareASElements)productCompare);
+    if(order->orderedProducts==NULL){
+      orderDestroy(order);
+      return NULL;
+    }
+    order->id=id;
+    return order;
+}
+
+
+void orderDestroy(Order order){
+    if(order==NULL){
+        return;
+    }
+    asDestroy(order->orderedProducts);
+    free(order);
+}
+
+
+Order orderCopy(Order order){
+    if(order==NULL){
+        return NULL;
+    }
+    Order newOrder=orderCreate(order->id);
+    if(newOrder==NULL){
+        return NULL;
+    }
+    AmountSet set_copy = asCopy(order->orderedProducts);
+    if(!set_copy){
+        orderDestroy(newOrder);
+        return NULL;
+    }
+    newOrder->orderedProducts = set_copy;
+    return newOrder;
+}
+
+
+    int orderCompare(Order or1, Order or2){
+        if(!or1 || !or2)
+            return -1;
+        return or1->id - or2->id;
+    }
+
+
